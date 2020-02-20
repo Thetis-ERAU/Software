6caf5b14efb28bdb66ffedfa3b9ded666e977bd0
mjliepke <42967384+mjliepke@users.noreply.github.com>
2020-02-20 13:33:03 -0500

    Adding GPS functionality and adding controller coordination code
    -Renamed SandSortingSystem SandSystem for ease of coding
    -included GPS functionality to InputSystem with basic try-catch loops
    -Included polling refresh capability to controller (update input values every _ second
    -Included HelperClasses.py, including RepeatedTimer class to run process every _ second
    -Added proper imports into Controller

--- PythonDev/Application/Triton/Controller.py
+++ PythonDev/Application/Triton/Controller.py
@@ -4,28 +4,48 @@ Created on Feb 4, 2020
 @author: matt
 '''
 from multiprocessing import Process
+import sched
+import time
+from InputSystem import *
+from DriveSystem import *
+from OpticalSystem import *
+from SandSystem import *
+from HelperClasses import RepeatedTimer
+
+
+inputRefreshPeriod = 2000 # 500 ms
+
 def DriveLoop():
-        '''
-        '''
-        print("in driveloop")
+    '''
+    '''
+    print("in driveloop")
+    driveSystem = DriveSystem(0)
         
 def OpticalLoop():
-        '''
-        '''
-        print("in opticalloop")
+    '''
+    '''
+    print("in opticalloop")
+    opticalSystem = OpticalSystem(0)
         
 def SandLoop():
-        '''
-        '''
-        print("in sandloop")
+    '''
+    '''
+    print("in sandloop")
+    sandSystem = SandSystem(0)
         
 def InputLoop():
-        '''
-        '''
-        print("in inputloop")
+    '''
+    '''
+    print("in inputloop")
+    inputSystem = InputSystem(inputRefreshPeriod)
+    refreshInputs = RepeatedTimer(inputRefreshPeriod, inputSystem.updateValues)
+
+  
+        
         
 #Start MultiProcessing Loops
 if __name__ == '__main__':
+
     # freeze_support()
     DriveProcess = Process(target = DriveLoop)
     OpticalProcess = Process(target = OpticalLoop)
--- PythonDev/Application/Triton/InputSystem.py
+++ PythonDev/Application/Triton/InputSystem.py
@@ -3,29 +3,83 @@ Created on Feb 4, 2020
 
 @author: matt
 '''
+import serial
+import adafruit_gps
+import time
+import sched
 
 class InputSystem(object):
     '''
     classdocs
     '''
 
-
-    def __init__(self, params):
+    def __init__(self, gpsFreq = 1000):
         '''
         Constructor
         '''
+        self.gpsFreq = gpsFreq  # in ms
+        try:
+            self.uart = serial.Serial('/dev/serial0', baudrate = 9600, timeout = 10)
+            self.gps = adafruit_gps.GPS(uart, debut = False)
+        except serial.SerialException:
+            self.gps = None
+            print("GPS connection failed, Serial Exception raised")
         
+        self.gpsDataDict = {'_uart': -1,
+                            'latitude': -1, 
+                            'longitude': -1, 
+                            'fix_quality': -1,
+                            'fix_quality_3d': -1,
+                            'altitude_m': -1,
+                            'debug': -1,
+                            'timestamp_utc': -1,
+                            'satellites': -1,
+                            'satellites_prev': -1,
+                            'speed_knots': -1,
+                            'height_geoid': -1,
+                            'track_angle_deg': -1,
+                            'sats': -1,
+                            'sel_mode': -1,
+                            'sat_prns': -1,
+                            'true_track': -1,
+                            'isactivedata': -1,
+                            }
+        self.gpsonline = self.setupGPS()
+
+
     def setupProperties(self):
         '''
         batteryislow & plasticFullProperties to be added
         @postcondition: Properties will not be null
         '''
         
-    def UpdateValues(self):
+    def updateValues(self):
         '''
         Updates all read values, and sends to log file
-        
         @postcondition: All input parameters will be updated with current values, or made null
         if unable to refresh.  Old Values will be sent to file
         @return: boolean status of excecution
         '''
+        allWorking = self.updateGPS()
+        return allWorking
+
+    def setupGPS(self):
+        if self.gps is None:
+            print("GPS has not been initialized, so SetupGPS in InputSystem cannot continue")
+            return False;
+        self.gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        self.gps.send_command(b'PMTK220,'+str(gpsFreq))
+    
+    def updateGPS(self):
+        if self.gps is None:
+            print("GPS has not been initialized, so updateGPS in InputSystem cannot continue")
+            return False;
+        self.gps.update()
+        if self.gps.has_fix:
+            for key in self.gpsDataDict.keys():
+                self.DataDict[key] = gps.key
+        else:
+            for key in self.gpsDataDict.keys():
+                self.DataDict[key] = -1
+
+
