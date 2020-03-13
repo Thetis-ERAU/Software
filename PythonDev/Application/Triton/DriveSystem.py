8928b0f376a86858ffa5652165877079ba6acc57
Matthew Liepke <LIEPKEM@my.erau.edu>
2020-03-13 10:45:42 -0400

    Updating Servo Tests & DriveSystem to test on RPI
    -ServoTest corrected for improper syntax
    -DriveSystem basic functionality implemented, to be tested on RPI

--- PythonDev/Application/Triton/DriveSystem.py
+++ PythonDev/Application/Triton/DriveSystem.py
@@ -3,21 +3,37 @@ Created on Feb 4, 2020
 
 @author: matt
 '''
+
+import board
+import busio
+import adafruit_pca9685
+import time
+
 class DriveSystem(object):
     '''
     classdocs
     '''
+    VICTOR_FREQ = 50                # Hz, freq of base signal
+    VICTOR_DUTY_MIN = 0x0ccd        # 5% of 0xffff in hex
+    VICTOR_DUTY_MAX = 0x1999        # 10% of 0xffff in hex
+    VICTOR_DUTY_NEUTRAL = 0x1333    # 7.5% of 0xffff in hex
 
-
-    def __init__(self, lServoPort = None, rServoPort = None):
+    def __init__(self, motorRightPort = 1, motorLeftPort = 2, i2c = None):
         '''
         Constructor
         '''
-
-        self.leftWheelServo = lServoPort
-        self.rightWheelServo = rServoPort
+        if i2c is None:
+            i2c = busio.I2C(board.SCL, board.SCA)
+        self.setupPwmHat(i2c, motorRightPort, motorLeftPort)
         
-    def TurnBot(self, angleRad, speed):
+    def setupPwmHat(self, i2c, rightPort, leftPort):
+        global VICTOR_FREQ
+        self.pwmHat = adafruit_pca9685.PCA9685(i2c)
+        self.pwmHat.frequency = VICTOR_FREQ
+        self.motorRightChannel = self.pwmHat.channels[rightPort]
+        self.motorLeftChannel = self.pwmHat.channels[leftPort]
+
+    def turnBot(self, angleRad, speed):
         ''' 
         Turns the Robot relative to the angleRad, changing the set speed to speed
         
@@ -27,7 +43,7 @@ class DriveSystem(object):
         ''' 
         return False
     
-    def ChangeSpeed(self, speed):
+    def changeSpeed(self, speed):
         '''
         Changes overall speed set of bot
         
@@ -35,19 +51,33 @@ class DriveSystem(object):
         @return state of execution
         '''
         
-    def FireLeftMotor(self,time):
+    def testFireVictor(self, motorChannel, seconds):
         '''
         Testing method that fires right motor for ___ time
         @return state of execution
         '''
-        
-    def FireRightMotor(self,time):
-        '''
-        Testing method that fires right motor for ___ time
-        @return state of execution
-        '''
+        global VICTOR_DUTY_MAX, VICTOR_DUTY_NEUTRAL
 
-    def GoStraight(self, doubleHere, doubleThere):
+        motorChannel.duty_cycle = VICTOR_DUTY_MAX
+        time.sleep(seconds)
+        motorChannel.duty_cycle = VICTOR_DUTY_NEUTRAL
+        
+
+    def fireRightMotor(self,seconds = 1):
+        '''
+        Testing method that fires right motor for ___ time
+        @return state of execution
+        '''
+        self.testFireVictor(self.motorRightChannel, seconds)
+
+    def fireLeftMotor(self, seconds = 1):
+        '''
+        Testing method that fires right motor for ___ time
+        @return state of execution
+        '''
+        self.testFireVictor(self.motorLeftChannel, seconds)
+
+    def goStraight(self, doubleHere, doubleThere):
         self.FireLeftMotor(doubleHere)
         self.FireRightMotor(doubleThere)
 
