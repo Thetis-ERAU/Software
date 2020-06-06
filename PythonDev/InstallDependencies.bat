7d6477c73d5426a7ac0de18a6ef71bd17522ac66
mjliepke <42967384+mjliepke@users.noreply.github.com>
2020-06-06 10:37:16 -0400

    Install .bat created to quickly get RPI running
    BAT does:
    -clones repo & puts in Desktop/Thetis
    -Installs adafruit dependencies (gps, gpio, ads, servohat, blinka)
    -enables required interfaces (cam, i2c, ssh, vnc, serial)

---
+++ PythonDev/InstallDependencies.bat
@@ -0,0 +1,27 @@
+#!bin/bash
+#Installs required dependencies for Thetis & clones repo
+
+#Clone Repo
+cd ~
+cd Desktop
+mkdir Thetis & cd "&_"
+git clone https://github.com/Thetis-ERAU/Software.git
+
+#Install dependencies
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install pip3
+sudo pip3 install adafruit-circuitpython-gps
+sudo pip3 install adafruit-circuitpython-servokit
+sudo pip3 install RPI.GPIO
+sudo pip3 install adafruit-blinka
+sudo pip3 insatll adafruit-circuitpython-ads1x15
+
+#sudo apt-get install gpsd gpsd-clients python-gps p
+
+#Enable VNC, Serial, I2C and SSH
+sudo raspi-config nonint do_ssh 0
+sudo raspi-config nonint do_camera 0
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_vnc 0
+sudo raspi-config noning do_serial 0
